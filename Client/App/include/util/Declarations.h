2a311dceeda4f3ba9682e80fb447d12153eeeecc
starfrost013 <mario64crashed@gmail.com>
2023-07-05 14:46:31 +0100

    add rbxinterface

---
+++ Client/App/include/util/Declarations.h
@@ -0,0 +1,2 @@
+#pragma once
+#define RBXInterface __declspec(novtable)
---
+++ Client/App/include/util/Log.h
@@ -0,0 +1,21 @@
+#pragma once
+#include "util/Declarations.h"
+namespace RBX
+{
+	class RBXInterface ILogProvider
+	{
+
+	};
+
+	class Log
+	{
+		enum LogSeverity
+		{
+			Success = 0,
+			Yield = 1,
+			Error = 2,
+		};
+
+
+	};
+}
